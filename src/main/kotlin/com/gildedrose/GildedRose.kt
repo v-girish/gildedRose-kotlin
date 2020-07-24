77094ba82c3b90daf1d1d11e2a0797fdc02338e3
Girish Verma <girish.verma026@gmail.com>
2020-07-25 00:16:39 +0530

    Extracts out method for updating quality for backstage passes item

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,36 +4,42 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
+            updateValueForBackstagePasses(items[i])
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 items[i].quality = degradedQualityFor(items[i])
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = incrementedQualityFor(items[i])
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            items[i].quality = incrementedQualityFor(items[i])
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            items[i].quality = incrementedQualityFor(items[i])
-                        }
-                    }
                 }
             }
 
             items[i].sellIn = newSellInValueFor(items[i])
 
             if (items[i].sellIn < 0) {
+                updateValueForBackstagePasses(items[i])
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                         items[i].quality = degradedQualityFor(items[i])
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
                     items[i].quality = incrementedQualityFor(items[i])
                 }
+            }
+        }
+    }
+
+    private fun updateValueForBackstagePasses(item: Item) {
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.sellIn < 11) {
+                item.quality = incrementedQualityFor(item)
+            }
+
+            if (item.sellIn < 6) {
+                item.quality = incrementedQualityFor(item)
+            }
+
+            if (item.sellIn < 0) {
+                item.quality = 0
             }
         }
     }
